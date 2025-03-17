3002dfc254f8bbd4bdd90f6ecf7abd2edefe1a5f
RHUAN CEZAR ALMEIDA DO CARMO <rhuan.cacarmo@senacsp.edu.br>
2025-03-17 17:08:41 -0300

    Alertas/MaiorQueDez

---
+++ maioroumenorquedez/Form1.Designer.cs
@@ -0,0 +1,76 @@
+﻿
+namespace maioroumenorquedez
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtMaiorOuMenorQue = new System.Windows.Forms.TextBox();
+            this.bntVerificar = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // txtMaiorOuMenorQue
+            // 
+            this.txtMaiorOuMenorQue.Location = new System.Drawing.Point(103, 64);
+            this.txtMaiorOuMenorQue.Name = "txtMaiorOuMenorQue";
+            this.txtMaiorOuMenorQue.Size = new System.Drawing.Size(96, 20);
+            this.txtMaiorOuMenorQue.TabIndex = 0;
+            this.txtMaiorOuMenorQue.Text = "Digite um número";
+            this.txtMaiorOuMenorQue.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // bntVerificar
+            // 
+            this.bntVerificar.Cursor = System.Windows.Forms.Cursors.Hand;
+            this.bntVerificar.Location = new System.Drawing.Point(113, 125);
+            this.bntVerificar.Name = "bntVerificar";
+            this.bntVerificar.Size = new System.Drawing.Size(75, 23);
+            this.bntVerificar.TabIndex = 2;
+            this.bntVerificar.Text = "Verificar";
+            this.bntVerificar.UseVisualStyleBackColor = true;
+            this.bntVerificar.Click += new System.EventHandler(this.bntVerificar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(322, 209);
+            this.Controls.Add(this.bntVerificar);
+            this.Controls.Add(this.txtMaiorOuMenorQue);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox txtMaiorOuMenorQue;
+        private System.Windows.Forms.Button bntVerificar;
+    }
+}
+
---
+++ maioroumenorquedez/Form1.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace maioroumenorquedez
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void bntVerificar_Click(object sender, EventArgs e)
+        {
+            if (int.TryParse(txtMaiorOuMenorQue.Text, out int numero))
+            {
+                if (numero >= 10)
+                {
+                    MessageBox.Show("Esse número é maior ou igual a dez","Maior",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+                else
+                {
+                    MessageBox.Show("Esse número é menor que dez","Menor",MessageBoxButtons.OK,MessageBoxIcon.Information);
+                }
+            }
+            else
+            {
+                // Se a conversão falhar, exibe uma mensagem de erro
+                MessageBox.Show("Por favor, insira um número válido.");
+            }
+        }
+    }
+}
