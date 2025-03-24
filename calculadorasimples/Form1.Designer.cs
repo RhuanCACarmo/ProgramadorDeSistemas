50e1fc840e089bcbd50b9f1cbc2a1c95df89dfa7
RHUAN CEZAR ALMEIDA DO CARMO <rhuan.cacarmo@senacsp.edu.br>
2025-03-24 17:30:43 -0300

    Atualização 24-03-25

---
+++ calculadorasimples/Form1.Designer.cs
@@ -0,0 +1,206 @@
+﻿
+namespace calculadorasimples
+{
+    partial class frmCalculadoraSimples
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.lblPrimeiroNumero = new System.Windows.Forms.Label();
+            this.txtPrimeiroNumero = new System.Windows.Forms.TextBox();
+            this.lblSegundoNumero = new System.Windows.Forms.Label();
+            this.txtSegundoNumero = new System.Windows.Forms.TextBox();
+            this.btnSoma = new System.Windows.Forms.Button();
+            this.lblResultado = new System.Windows.Forms.Label();
+            this.lblProdutoCalculo = new System.Windows.Forms.Label();
+            this.btnSubtrair = new System.Windows.Forms.Button();
+            this.btnMultiplicar = new System.Windows.Forms.Button();
+            this.btnDividir = new System.Windows.Forms.Button();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.btnDividir);
+            this.groupBox1.Controls.Add(this.btnMultiplicar);
+            this.groupBox1.Controls.Add(this.btnSubtrair);
+            this.groupBox1.Controls.Add(this.lblProdutoCalculo);
+            this.groupBox1.Controls.Add(this.lblResultado);
+            this.groupBox1.Controls.Add(this.btnSoma);
+            this.groupBox1.Controls.Add(this.txtSegundoNumero);
+            this.groupBox1.Controls.Add(this.lblSegundoNumero);
+            this.groupBox1.Controls.Add(this.txtPrimeiroNumero);
+            this.groupBox1.Controls.Add(this.lblPrimeiroNumero);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(248, 396);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            // 
+            // lblPrimeiroNumero
+            // 
+            this.lblPrimeiroNumero.AutoSize = true;
+            this.lblPrimeiroNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblPrimeiroNumero.Location = new System.Drawing.Point(57, 31);
+            this.lblPrimeiroNumero.Name = "lblPrimeiroNumero";
+            this.lblPrimeiroNumero.Size = new System.Drawing.Size(126, 20);
+            this.lblPrimeiroNumero.TabIndex = 0;
+            this.lblPrimeiroNumero.Text = "Primeiro Número";
+            // 
+            // txtPrimeiroNumero
+            // 
+            this.txtPrimeiroNumero.Location = new System.Drawing.Point(69, 54);
+            this.txtPrimeiroNumero.Name = "txtPrimeiroNumero";
+            this.txtPrimeiroNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtPrimeiroNumero.TabIndex = 1;
+            // 
+            // lblSegundoNumero
+            // 
+            this.lblSegundoNumero.AutoSize = true;
+            this.lblSegundoNumero.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblSegundoNumero.Location = new System.Drawing.Point(57, 82);
+            this.lblSegundoNumero.Name = "lblSegundoNumero";
+            this.lblSegundoNumero.Size = new System.Drawing.Size(134, 20);
+            this.lblSegundoNumero.TabIndex = 2;
+            this.lblSegundoNumero.Text = "Segundo Número";
+            this.lblSegundoNumero.Click += new System.EventHandler(this.lblSegundoNumero_Click);
+            // 
+            // txtSegundoNumero
+            // 
+            this.txtSegundoNumero.Location = new System.Drawing.Point(69, 105);
+            this.txtSegundoNumero.Name = "txtSegundoNumero";
+            this.txtSegundoNumero.Size = new System.Drawing.Size(100, 20);
+            this.txtSegundoNumero.TabIndex = 3;
+            this.txtSegundoNumero.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // btnSoma
+            // 
+            this.btnSoma.BackColor = System.Drawing.Color.ForestGreen;
+            this.btnSoma.FlatAppearance.BorderSize = 0;
+            this.btnSoma.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSoma.ForeColor = System.Drawing.Color.White;
+            this.btnSoma.Location = new System.Drawing.Point(69, 149);
+            this.btnSoma.Name = "btnSoma";
+            this.btnSoma.Size = new System.Drawing.Size(100, 35);
+            this.btnSoma.TabIndex = 4;
+            this.btnSoma.Text = "Somar";
+            this.btnSoma.UseVisualStyleBackColor = false;
+            this.btnSoma.Click += new System.EventHandler(this.btnSoma_Click);
+            // 
+            // lblResultado
+            // 
+            this.lblResultado.AutoSize = true;
+            this.lblResultado.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblResultado.Location = new System.Drawing.Point(6, 324);
+            this.lblResultado.Name = "lblResultado";
+            this.lblResultado.Size = new System.Drawing.Size(86, 20);
+            this.lblResultado.TabIndex = 5;
+            this.lblResultado.Text = "Resultado:";
+            // 
+            // lblProdutoCalculo
+            // 
+            this.lblProdutoCalculo.AutoSize = true;
+            this.lblProdutoCalculo.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblProdutoCalculo.Location = new System.Drawing.Point(98, 320);
+            this.lblProdutoCalculo.Name = "lblProdutoCalculo";
+            this.lblProdutoCalculo.Size = new System.Drawing.Size(24, 25);
+            this.lblProdutoCalculo.TabIndex = 6;
+            this.lblProdutoCalculo.Text = "0";
+            // 
+            // btnSubtrair
+            // 
+            this.btnSubtrair.BackColor = System.Drawing.Color.Crimson;
+            this.btnSubtrair.FlatAppearance.BorderSize = 0;
+            this.btnSubtrair.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnSubtrair.ForeColor = System.Drawing.Color.White;
+            this.btnSubtrair.Location = new System.Drawing.Point(69, 190);
+            this.btnSubtrair.Name = "btnSubtrair";
+            this.btnSubtrair.Size = new System.Drawing.Size(100, 34);
+            this.btnSubtrair.TabIndex = 7;
+            this.btnSubtrair.Text = "Subtrair";
+            this.btnSubtrair.UseVisualStyleBackColor = false;
+            this.btnSubtrair.Click += new System.EventHandler(this.btnSubtrair_Click);
+            // 
+            // btnMultiplicar
+            // 
+            this.btnMultiplicar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnMultiplicar.FlatAppearance.BorderSize = 0;
+            this.btnMultiplicar.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnMultiplicar.ForeColor = System.Drawing.Color.White;
+            this.btnMultiplicar.Location = new System.Drawing.Point(69, 230);
+            this.btnMultiplicar.Name = "btnMultiplicar";
+            this.btnMultiplicar.Size = new System.Drawing.Size(100, 33);
+            this.btnMultiplicar.TabIndex = 8;
+            this.btnMultiplicar.Text = "Multiplicar";
+            this.btnMultiplicar.UseVisualStyleBackColor = false;
+            this.btnMultiplicar.Click += new System.EventHandler(this.btnMultiplicar_Click);
+            // 
+            // btnDividir
+            // 
+            this.btnDividir.BackColor = System.Drawing.Color.Goldenrod;
+            this.btnDividir.FlatAppearance.BorderSize = 0;
+            this.btnDividir.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.btnDividir.Location = new System.Drawing.Point(69, 269);
+            this.btnDividir.Name = "btnDividir";
+            this.btnDividir.Size = new System.Drawing.Size(100, 32);
+            this.btnDividir.TabIndex = 9;
+            this.btnDividir.Text = "Dividir";
+            this.btnDividir.UseVisualStyleBackColor = false;
+            this.btnDividir.Click += new System.EventHandler(this.btnDividir_Click);
+            // 
+            // frmCalculadoraSimples
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(272, 420);
+            this.Controls.Add(this.groupBox1);
+            this.MaximizeBox = false;
+            this.Name = "frmCalculadoraSimples";
+            this.Text = "Calculadora Simples";
+            this.Load += new System.EventHandler(this.frmCalculadoraSimples_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox txtSegundoNumero;
+        private System.Windows.Forms.Label lblSegundoNumero;
+        private System.Windows.Forms.TextBox txtPrimeiroNumero;
+        private System.Windows.Forms.Label lblPrimeiroNumero;
+        private System.Windows.Forms.Button btnSoma;
+        private System.Windows.Forms.Label lblProdutoCalculo;
+        private System.Windows.Forms.Label lblResultado;
+        private System.Windows.Forms.Button btnSubtrair;
+        private System.Windows.Forms.Button btnDividir;
+        private System.Windows.Forms.Button btnMultiplicar;
+    }
+}
+
---
+++ calculadorasimples/Form1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace calculadorasimples
+{
+    public partial class frmCalculadoraSimples : Form
+    {
+        public frmCalculadoraSimples()
+        {
+            InitializeComponent();
+        }
+
+        private void frmCalculadoraSimples_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void lblSegundoNumero_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnSoma_Click(object sender, EventArgs e)
+        {
+            lblProdutoCalculo.Text = (float.Parse(txtPrimeiroNumero.Text) + float.Parse(txtSegundoNumero.Text)).ToString();
+        }
+
+        private void btnSubtrair_Click(object sender, EventArgs e)
+        {
+            lblProdutoCalculo.Text = (float.Parse(txtPrimeiroNumero.Text) - float.Parse(txtSegundoNumero.Text)).ToString();
+        }
+
+        private void btnMultiplicar_Click(object sender, EventArgs e)
+        {
+            lblProdutoCalculo.Text = (float.Parse(txtPrimeiroNumero.Text) * float.Parse(txtSegundoNumero.Text)).ToString();
+        }
+
+        private void btnDividir_Click(object sender, EventArgs e)
+        {
+            lblProdutoCalculo.Text = (float.Parse(txtPrimeiroNumero.Text) / float.Parse(txtSegundoNumero.Text)).ToString();
+        }
+    }
+}
