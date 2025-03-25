1419373dd621db07538cde01a44f8ed94dd9c69a
RHUAN CEZAR ALMEIDA DO CARMO <rhuan.cacarmo@senacsp.edu.br>
2025-03-25 14:32:01 -0300

    Atualização 25-03-25

---
+++ arrayunidimensional-vetor/Form1.Designer.cs
@@ -0,0 +1,123 @@
+﻿
+namespace arrayunidimensional_vetor
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.panel2 = new System.Windows.Forms.Panel();
+            this.lblTopo = new System.Windows.Forms.Label();
+            this.lblBase = new System.Windows.Forms.Label();
+            this.btnTestar = new System.Windows.Forms.Button();
+            this.panel1.SuspendLayout();
+            this.panel2.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // panel1
+            // 
+            this.panel1.BackColor = System.Drawing.Color.Navy;
+            this.panel1.Controls.Add(this.lblTopo);
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.panel1.Location = new System.Drawing.Point(0, 0);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(800, 100);
+            this.panel1.TabIndex = 0;
+            // 
+            // panel2
+            // 
+            this.panel2.BackColor = System.Drawing.Color.Navy;
+            this.panel2.Controls.Add(this.lblBase);
+            this.panel2.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel2.Location = new System.Drawing.Point(0, 350);
+            this.panel2.Name = "panel2";
+            this.panel2.Size = new System.Drawing.Size(800, 100);
+            this.panel2.TabIndex = 1;
+            // 
+            // lblTopo
+            // 
+            this.lblTopo.AutoSize = true;
+            this.lblTopo.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblTopo.ForeColor = System.Drawing.Color.White;
+            this.lblTopo.Location = new System.Drawing.Point(142, 34);
+            this.lblTopo.Name = "lblTopo";
+            this.lblTopo.Size = new System.Drawing.Size(516, 31);
+            this.lblTopo.TabIndex = 0;
+            this.lblTopo.Text = "Para testar o Array unidimensional - Vetor";
+            // 
+            // lblBase
+            // 
+            this.lblBase.AutoSize = true;
+            this.lblBase.Font = new System.Drawing.Font("Microsoft Sans Serif", 20.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.lblBase.ForeColor = System.Drawing.Color.White;
+            this.lblBase.Location = new System.Drawing.Point(244, 39);
+            this.lblBase.Name = "lblBase";
+            this.lblBase.Size = new System.Drawing.Size(282, 31);
+            this.lblBase.TabIndex = 1;
+            this.lblBase.Text = "Clique no botão acima";
+            // 
+            // btnTestar
+            // 
+            this.btnTestar.BackColor = System.Drawing.Color.RoyalBlue;
+            this.btnTestar.Font = new System.Drawing.Font("Microsoft Sans Serif", 36F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnTestar.ForeColor = System.Drawing.Color.White;
+            this.btnTestar.Location = new System.Drawing.Point(289, 191);
+            this.btnTestar.Name = "btnTestar";
+            this.btnTestar.Size = new System.Drawing.Size(227, 60);
+            this.btnTestar.TabIndex = 2;
+            this.btnTestar.Text = "OK";
+            this.btnTestar.UseVisualStyleBackColor = false;
+            this.btnTestar.Click += new System.EventHandler(this.btnTestar_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnTestar);
+            this.Controls.Add(this.panel2);
+            this.Controls.Add(this.panel1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.panel1.ResumeLayout(false);
+            this.panel1.PerformLayout();
+            this.panel2.ResumeLayout(false);
+            this.panel2.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Panel panel1;
+        private System.Windows.Forms.Label lblTopo;
+        private System.Windows.Forms.Panel panel2;
+        private System.Windows.Forms.Label lblBase;
+        private System.Windows.Forms.Button btnTestar;
+    }
+}
+
---
+++ arrayunidimensional-vetor/Form1.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace arrayunidimensional_vetor
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void btnTestar_Click(object sender, EventArgs e)
+        {
+            string[] pecasComputador = { "Mouse", "Teclado", "Monitor", "Gabinete", "Câmera" };
+
+            //MessageBox.Show(pecasComputador[2],"Peça Escolhida");
+
+            foreach (string peca in pecasComputador)
+                MessageBox.Show(peca);
+        }
+    }
+}
